60c16e30d44f0e26cbfef92bfb6703a9a7ca3d4e
Josh Marler <jmarler@inmusibrands.com>
2020-07-19 14:01:48 +0100

    Add basic DelayPlugin JSUI layout

--- Source/jsui/src/App.js
+++ Source/jsui/src/App.js
@@ -4,15 +4,45 @@ import {
   Text,
 } from 'juce-blueprint';
 
+import { Colors } from './Constants'
+import Slider from './Slider'
+import Label from './Label'
+import ParameterToggleButton from './ParameterToggleButton'
+
 
 class App extends Component {
   render() {
     return (
       <View {...styles.container}>
         <View {...styles.content}>
-          <Text {...styles.text}>
-            Now Elias needs to add some things.
-          </Text>
+          <View {...styles.header_content}>
+            <Text {...styles.header_text}>
+              Delay Plugin
+            </Text>
+          </View>
+          <View {...styles.sliders_content}>
+            <View {... styles.slider_content}>
+              <Slider paramId="DelayTime" {...styles.slider}>
+                <Label paramId="DelayTime" {...styles.slider_value_label} />
+              </Slider>
+              <Text {...styles.slider_name_label} {...styles.text}>
+                Delay Time
+              </Text>
+            </View>
+            <View {... styles.slider_content}>
+              <Slider paramId="DelayDryWet" {...styles.slider}>
+                <Label paramId="DelayDryWet" {...styles.slider_value_label} />
+              </Slider>
+              <Text {...styles.slider_name_label} {...styles.text}>
+                Dry/Wet
+              </Text>
+            </View>
+          </View>
+          <View {...styles.buttons_content}>
+            <ParameterToggleButton {...styles.button}>
+              <Text {...styles.text}>Sync</Text>
+            </ParameterToggleButton>
+          </View>
         </View>
       </View>
     );
@@ -27,13 +57,67 @@ const styles = {
   },
   content: {
     'flex': 1.0,
+    'flex-direction': 'column',
     'justify-content': 'center',
     'align-items': 'center',
+  },
+  header_content: {
+    'justify-content': 'center',
+    'align-items': 'center',
+    'height': "10%"
+  },
+  header_text: {
+    'font-size': 20.0,
+    'line-spacing': 1.6,
+    'color': 'ff626262'
+  },
+  sliders_content: {
+    'justify-content': 'space-around',
+    'width': '100%',
+    'height': "60%"
+  },
+  slider_content: {
+    'flex': 1.0,
+    'flex-direction': 'column',
+    'justify-content': 'center',
+    'align-items': 'center',
+  },
+  slider: {
+    'min-width': 100.0,
+    'min-height': 100.0,
+    'height': '50%',
+    'width': '50%',
+  },
+  slider_value_label: {
+    'flex': 1.0,
+    'justify-content': 'center',
+    'align-items': 'center',
+    'interceptClickEvents': false
+  },
+  slider_name_label: {
+    'justify-content': 'center',
+    'align-items': 'center',
+    'interceptClickEvents': false
+  },
+  buttons_content: {
+    'width': "100%",
+    'align-items': 'center',
+    'justify-content': 'space-around',
+    'height': "20%"
   },
   text: {
     'font-size': 18.0,
     'line-spacing': 1.6,
     'color': 'ff626262',
+  },
+  button: {
+    'height': '100%',
+    'justify-content': 'center',
+    'align-items': 'center',
+    'aspect-ratio': 1.0,
+    'border-width': 2.0,
+    'border-color': Colors.STROKE,
+    'border-radius': '20%',
   },
 };
 
